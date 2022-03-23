18109c56fd620c42b6cb02f65ed267ffd1c7a587
Ghias ali <ghiasali24@gmail.com>
2022-03-23 23:56:52 +0500

    updated details ui

--- src/components/details.vue
+++ src/components/details.vue
@@ -1,16 +1,34 @@
 <template>
  <div >
-     <button @click="goto('/')">Home</button>
-          <h3>Details</h3>
+         
+          <div class="container text-start" >
+                   <button @click="goto('/')" class="btn btn-info">Home</button>
+
+                <h3 class="text-start mt-2">Add Item to Cart</h3>
+         
           <div class="row">
-              <div class="col-lg-6">
-                  <img :src="details.url" alt="">
+              <div class="col-lg-3 text-start">
+                  <img :src="details.url" alt="" >
+
               </div>
-              <div class="col-lg-6 bg-info">
-                  <h5>{{details.name}}</h5>
-                  <p>Price:{{details.price}}</p>
+              <div class="col-lg-4  text-start">
+                  <h5 class=" fw-600 ">Name:{{details.name}}</h5>
+                  <h5 class="my-4 fw-600"> Price:{{details.price}}</h5>
+                 <h5 class="my-4 fw-600"> Description:{{details.desc}}</h5>
+                <h5 class="my-4 fw-600"> Rating:{{details.rate}}</h5>
+
+                <div class=" d-flex ">
+                <button class="btn btn-info m-1 ">Go to cart</button>
+                <button class="btn btn-success m-1">Add item</button>
+                <button class="btn btn-danger m-1">Remove item</button>
+                </div>
+                
+
               </div>
           </div>
+          
+           </div>
+
  </div>
 </template>
 
@@ -25,7 +43,17 @@ export default {
         goto(url) {
             this.$router.push(url)
         },
+       
     },
+     created(){
+         console.log('created')
+            if(this.$route.params.id!==undefined)
+            localStorage.setItem("details",JSON.stringify(this.$route.params))
+        },
+        mounted(){
+              console.log('mounted')
+            this.details=JSON.parse(localStorage.getItem('details'))
+        }
 }
 </script>
 
